7c744ae79f8319254737fd6a94eeb0a143537c18
pentantan <shutux500@gmail.com>
2019-06-28 05:13:09 +0900

    format fixed

--- src/main/java/twilightforest/entity/EntityTFIceExploder.java
+++ src/main/java/twilightforest/entity/EntityTFIceExploder.java
@@ -30,185 +30,185 @@ import twilightforest.client.particle.TFParticleType;
 
 public class EntityTFIceExploder extends EntityMob {
 
-        public static final ResourceLocation LOOT_TABLE = TwilightForestMod.prefix("entities/ice_exploder");
-        private static final float EXPLOSION_RADIUS = 1;
+	public static final ResourceLocation LOOT_TABLE = TwilightForestMod.prefix("entities/ice_exploder");
+	private static final float EXPLOSION_RADIUS = 1;
 
-        public EntityTFIceExploder(World world) {
-                super(world);
-                this.setSize(0.8F, 1.8F);
-        }
+	public EntityTFIceExploder(World world) {
+		super(world);
+		this.setSize(0.8F, 1.8F);
+	}
 
-        @Override
-        protected void initEntityAI() {
-                this.tasks.addTask(0, new EntityAISwimming(this));
-                this.tasks.addTask(1, new EntityAIAttackMelee(this, 1.0D, false));
-                this.tasks.addTask(2, new EntityAIWander(this, 1.0D));
-                this.tasks.addTask(3, new EntityAIWatchClosest(this, EntityPlayer.class, 8.0F));
-                this.tasks.addTask(3, new EntityAILookIdle(this));
-                this.targetTasks.addTask(1, new EntityAIHurtByTarget(this, true));
-                this.targetTasks.addTask(2, new EntityAINearestAttackableTarget<>(this, EntityPlayer.class, true));
-        }
+	@Override
+	protected void initEntityAI() {
+		this.tasks.addTask(0, new EntityAISwimming(this));
+		this.tasks.addTask(1, new EntityAIAttackMelee(this, 1.0D, false));
+		this.tasks.addTask(2, new EntityAIWander(this, 1.0D));
+		this.tasks.addTask(3, new EntityAIWatchClosest(this, EntityPlayer.class, 8.0F));
+		this.tasks.addTask(3, new EntityAILookIdle(this));
+		this.targetTasks.addTask(1, new EntityAIHurtByTarget(this, true));
+		this.targetTasks.addTask(2, new EntityAINearestAttackableTarget<>(this, EntityPlayer.class, true));
+	}
 
-        @Override
-        protected void applyEntityAttributes() {
-                super.applyEntityAttributes();
-                this.getEntityAttribute(SharedMonsterAttributes.MOVEMENT_SPEED).setBaseValue(0.23000000417232513D);
-                this.getEntityAttribute(SharedMonsterAttributes.ATTACK_DAMAGE).setBaseValue(3.0D);
-        }
+	@Override
+	protected void applyEntityAttributes() {
+		super.applyEntityAttributes();
+		this.getEntityAttribute(SharedMonsterAttributes.MOVEMENT_SPEED).setBaseValue(0.23000000417232513D);
+		this.getEntityAttribute(SharedMonsterAttributes.ATTACK_DAMAGE).setBaseValue(3.0D);
+	}
 
-        @Override
-        public ResourceLocation getLootTable() {
-                return LOOT_TABLE;
-        }
+	@Override
+	public ResourceLocation getLootTable() {
+		return LOOT_TABLE;
+	}
 
-        @Override
-        public void onLivingUpdate() {
-                super.onLivingUpdate();
-                if (this.world.isRemote) {
-                        // make snow particles
-                        for (int i = 0; i < 3; i++) {
-                                float px = (this.rand.nextFloat() - this.rand.nextFloat()) * 0.3F;
-                                float py = this.getEyeHeight() + (this.rand.nextFloat() - this.rand.nextFloat()) * 0.5F;
-                                float pz = (this.rand.nextFloat() - this.rand.nextFloat()) * 0.3F;
+	@Override
+	public void onLivingUpdate() {
+		super.onLivingUpdate();
+		if (this.world.isRemote) {
+			// make snow particles
+			for (int i = 0; i < 3; i++) {
+				float px = (this.rand.nextFloat() - this.rand.nextFloat()) * 0.3F;
+				float py = this.getEyeHeight() + (this.rand.nextFloat() - this.rand.nextFloat()) * 0.5F;
+				float pz = (this.rand.nextFloat() - this.rand.nextFloat()) * 0.3F;
 
-                                TwilightForestMod.proxy.spawnParticle(TFParticleType.SNOW_GUARDIAN, this.lastTickPosX + px, this.lastTickPosY + py, this.lastTickPosZ + pz, 0, 0, 0);
-                                if (this.world.getBiome(this.getPosition()).getTemperature(this.getPosition()) > 1.0F || this.isBurning()) {
-                                        this.world.spawnParticle(EnumParticleTypes.CLOUD, this.lastTickPosX + px, this.lastTickPosY + py, this.lastTickPosZ + pz, 0, 0.1F, 0);
-                                        this.world.spawnParticle(EnumParticleTypes.WATER_DROP, this.lastTickPosX + px, this.lastTickPosY + py, this.lastTickPosZ + pz, 0, 0, 0);
-                                }
-                        }
-                }
-                if (this.world.getBiome(this.getPosition()).getTemperature(this.getPosition()) > 1.0F && this.ticksExisted % 20 == 0) {
-                        //BURN!!!
-                        this.attackEntityFrom(DamageSource.ON_FIRE, 1.0F);
-                }
-        }
+				TwilightForestMod.proxy.spawnParticle(TFParticleType.SNOW_GUARDIAN, this.lastTickPosX + px, this.lastTickPosY + py, this.lastTickPosZ + pz, 0, 0, 0);
+				if (this.world.getBiome(this.getPosition()).getTemperature(this.getPosition()) > 1.0F || this.isBurning()) {
+					this.world.spawnParticle(EnumParticleTypes.CLOUD, this.lastTickPosX + px, this.lastTickPosY + py, this.lastTickPosZ + pz, 0, 0.1F, 0);
+					this.world.spawnParticle(EnumParticleTypes.WATER_DROP, this.lastTickPosX + px, this.lastTickPosY + py, this.lastTickPosZ + pz, 0, 0, 0);
+				}
+			}
+		}
+		if (this.world.getBiome(this.getPosition()).getTemperature(this.getPosition()) > 1.0F && this.ticksExisted % 20 == 0) {
+			//BURN!!!
+			this.attackEntityFrom(DamageSource.ON_FIRE, 1.0F);
+		}
+	}
 
-        @Override
-        protected SoundEvent getAmbientSound() {
-                return TFSounds.ICE_AMBIENT;
-        }
+	@Override
+	protected SoundEvent getAmbientSound() {
+		return TFSounds.ICE_AMBIENT;
+	}
 
-        @Override
-        protected SoundEvent getHurtSound(DamageSource source) {
-                return TFSounds.ICE_HURT;
-        }
+	@Override
+	protected SoundEvent getHurtSound(DamageSource source) {
+		return TFSounds.ICE_HURT;
+	}
 
-        @Override
-        protected SoundEvent getDeathSound() {
-                return TFSounds.ICE_DEATH;
-        }
+	@Override
+	protected SoundEvent getDeathSound() {
+		return TFSounds.ICE_DEATH;
+	}
 
-        @Override
-        public float getEyeHeight() {
-                return this.height * 0.6F;
-        }
+	@Override
+	public float getEyeHeight() {
+		return this.height * 0.6F;
+	}
 
-        @Override
-        protected void onDeathUpdate() {
-                ++this.deathTime;
+	@Override
+	protected void onDeathUpdate() {
+		++this.deathTime;
 
-                if (this.deathTime == 60) // delay until 3 seconds
-                {
-                        if (!world.isRemote) {
-                                boolean mobGriefing = ForgeEventFactory.getMobGriefingEvent(world, this);
-                                this.world.createExplosion(this, this.posX, this.posY, this.posZ, EntityTFIceExploder.EXPLOSION_RADIUS, mobGriefing);
+		if (this.deathTime == 60) // delay until 3 seconds
+		{
+			if (!world.isRemote) {
+				boolean mobGriefing = ForgeEventFactory.getMobGriefingEvent(world, this);
+				this.world.createExplosion(this, this.posX, this.posY, this.posZ, EntityTFIceExploder.EXPLOSION_RADIUS, mobGriefing);
 
-                                if (mobGriefing) {
-                                        this.transformBlocks();
-                                }
-                        }
-                        // Fake to trigger super's behaviour
-                        deathTime = 19;
-                        super.onDeathUpdate();
-                        deathTime = 60;
-                }
-        }
+				if (mobGriefing) {
+					this.transformBlocks();
+				}
+			}
+			// Fake to trigger super's behaviour
+			deathTime = 19;
+			super.onDeathUpdate();
+			deathTime = 60;
+		}
+	}
 
-        private void transformBlocks() {
-                int range = 4;
+	private void transformBlocks() {
+		int range = 4;
 
-                BlockPos pos = new BlockPos(this);
+		BlockPos pos = new BlockPos(this);
 
-                for (int dx = -range; dx <= range; dx++) {
-                        for (int dy = -range; dy <= range; dy++) {
-                                for (int dz = -range; dz <= range; dz++) {
-                                        double distance = Math.sqrt(dx * dx + dy * dy + dz * dz);
+		for (int dx = -range; dx <= range; dx++) {
+			for (int dy = -range; dy <= range; dy++) {
+				for (int dz = -range; dz <= range; dz++) {
+					double distance = Math.sqrt(dx * dx + dy * dy + dz * dz);
 
-                                        float randRange = range + (rand.nextFloat() - rand.nextFloat()) * 2F;
+					float randRange = range + (rand.nextFloat() - rand.nextFloat()) * 2F;
 
-                                        if (distance < randRange) {
-                                                this.transformBlock(pos.add(dx, dy, dz));
-                                        }
-                                }
-                        }
-                }
-        }
-
-
-        private void transformBlock(BlockPos pos) {
-                IBlockState state = world.getBlockState(pos);
-                Block block = state.getBlock();
-
-                // check if we should even explode this
-                if (block.getExplosionResistance(this) < 8F && state.getBlockHardness(world, pos) >= 0) {
-                        // todo improve for blocks where state is known? or perhaps if a propertycolor is present
-                        int blockColor = state.getMapColor(world, pos).colorValue;
-
-                        // do appropriate transformation
-                        if (this.shouldTransformGlass(state, pos)) {
-                                this.world.setBlockState(pos, Blocks.STAINED_GLASS.getDefaultState().withProperty(BlockStainedGlass.COLOR, getClosestDyeColor(blockColor)));
-                        } else if (this.shouldTransformClay(state, pos)) {
-                                this.world.setBlockState(pos, Blocks.STAINED_HARDENED_CLAY.getDefaultState().withProperty(BlockColored.COLOR, getClosestDyeColor(blockColor)));
-                        }
-                }
-        }
+					if (distance < randRange) {
+						this.transformBlock(pos.add(dx, dy, dz));
+					}
+				}
+			}
+		}
+	}
 
 
-        private boolean shouldTransformClay(IBlockState state, BlockPos pos) {
-                return state.getBlock().isNormalCube(state, this.world, pos);
-        }
+	private void transformBlock(BlockPos pos) {
+		IBlockState state = world.getBlockState(pos);
+		Block block = state.getBlock();
+
+		// check if we should even explode this
+		if (block.getExplosionResistance(this) < 8F && state.getBlockHardness(world, pos) >= 0) {
+			// todo improve for blocks where state is known? or perhaps if a propertycolor is present
+			int blockColor = state.getMapColor(world, pos).colorValue;
+
+			// do appropriate transformation
+			if (this.shouldTransformGlass(state, pos)) {
+				this.world.setBlockState(pos, Blocks.STAINED_GLASS.getDefaultState().withProperty(BlockStainedGlass.COLOR, getClosestDyeColor(blockColor)));
+			} else if (this.shouldTransformClay(state, pos)) {
+				this.world.setBlockState(pos, Blocks.STAINED_HARDENED_CLAY.getDefaultState().withProperty(BlockColored.COLOR, getClosestDyeColor(blockColor)));
+			}
+		}
+	}
 
 
-        private boolean shouldTransformGlass(IBlockState state, BlockPos pos) {
-                return state.getBlock() != Blocks.AIR && this.isBlockNormalBounds(state, pos) && (!state.getMaterial().isOpaque() || state.getBlock().isLeaves(state, this.world, pos) || state.getBlock() == Blocks.ICE || state.getBlock() == TFBlocks.aurora_block);
-        }
+	private boolean shouldTransformClay(IBlockState state, BlockPos pos) {
+		return state.getBlock().isNormalCube(state, this.world, pos);
+	}
 
 
-        private boolean isBlockNormalBounds(IBlockState state, BlockPos pos) {
-                return Block.FULL_BLOCK_AABB.equals(state.getBoundingBox(world, pos));
-        }
+	private boolean shouldTransformGlass(IBlockState state, BlockPos pos) {
+		return state.getBlock() != Blocks.AIR && this.isBlockNormalBounds(state, pos) && (!state.getMaterial().isOpaque() || state.getBlock().isLeaves(state, this.world, pos) || state.getBlock() == Blocks.ICE || state.getBlock() == TFBlocks.aurora_block);
+	}
 
 
-        private EnumDyeColor getClosestDyeColor(int blockColor) {
-                int red = (blockColor >> 16) & 255;
-                int green = (blockColor >> 8) & 255;
-                int blue = blockColor & 255;
+	private boolean isBlockNormalBounds(IBlockState state, BlockPos pos) {
+		return Block.FULL_BLOCK_AABB.equals(state.getBoundingBox(world, pos));
+	}
 
 
-                EnumDyeColor bestColor = EnumDyeColor.WHITE;
-                int bestDifference = 1024;
+	private EnumDyeColor getClosestDyeColor(int blockColor) {
+		int red = (blockColor >> 16) & 255;
+		int green = (blockColor >> 8) & 255;
+		int blue = blockColor & 255;
 
-                for (EnumDyeColor color : EnumDyeColor.values()) {
-                        float[] iColor = color.getColorComponentValues();
 
-                        int iRed = (int) (iColor[0] * 255F);
-                        int iGreen = (int) (iColor[1] * 255F);
-                        int iBlue = (int) (iColor[2] * 255F);
+		EnumDyeColor bestColor = EnumDyeColor.WHITE;
+		int bestDifference = 1024;
 
-                        int difference = Math.abs(red - iRed) + Math.abs(green - iGreen) + Math.abs(blue - iBlue);
+		for (EnumDyeColor color : EnumDyeColor.values()) {
+			float[] iColor = color.getColorComponentValues();
 
-                        if (difference < bestDifference) {
-                                bestColor = color;
-                                bestDifference = difference;
-                        }
-                }
+			int iRed = (int) (iColor[0] * 255F);
+			int iGreen = (int) (iColor[1] * 255F);
+			int iBlue = (int) (iColor[2] * 255F);
 
-                return bestColor;
-        }
+			int difference = Math.abs(red - iRed) + Math.abs(green - iGreen) + Math.abs(blue - iBlue);
 
-        @Override
-        public int getMaxSpawnedInChunk() {
-                return 8;
-        }
+			if (difference < bestDifference) {
+				bestColor = color;
+				bestDifference = difference;
+			}
+		}
+
+		return bestColor;
+	}
+
+	@Override
+	public int getMaxSpawnedInChunk() {
+		return 8;
+	}
 }
