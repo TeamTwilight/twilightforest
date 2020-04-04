80f061b4fea654b40321d8a8047bbe561372a1d4
Jon McManus <due@wxwhatever.com>
2020-04-05 08:20:35 +1000

    Reindent.

--- src/main/java/twilightforest/entity/MultiPartEntityPart.java
+++ src/main/java/twilightforest/entity/MultiPartEntityPart.java
@@ -2,60 +2,60 @@ package twilightforest.entity;
 
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntitySize;
-import net.minecraft.entity.EntityType;
 import net.minecraft.entity.Pose;
 import net.minecraft.nbt.CompoundNBT;
 import net.minecraft.network.IPacket;
 import net.minecraft.util.DamageSource;
+import net.minecraftforge.fml.network.NetworkHooks;
 
 public class MultiPartEntityPart<T extends Entity & IEntityMultiPart> extends Entity {
-  public final T parent;
-  public final String partName;
+	public final T parent;
+	public final String partName;
 
-  public MultiPartEntityPart(T parent, String partName, float width, float height) {
-    super(parent.getType(), parent.world);
-    this.parent = parent;
-    this.partName = partName;
-    this.size = EntitySize.flexible(width, height);
-    this.recalculateSize();
-  }
+	public MultiPartEntityPart(T parent, String partName, float width, float height) {
+		super(parent.getType(), parent.world);
+		this.parent = parent;
+		this.partName = partName;
+		this.size = EntitySize.flexible(width, height);
+		this.recalculateSize();
+	}
 
-  @Override
-  protected void registerData() {
-  }
+	@Override
+	protected void registerData() {
+	}
 
-  @Override
-  protected void readAdditional(CompoundNBT tag) {
-  }
+	@Override
+	protected void readAdditional(CompoundNBT tag) {
+	}
 
-  @Override
-  protected void writeAdditional(CompoundNBT tag) {
-  }
+	@Override
+	protected void writeAdditional(CompoundNBT tag) {
+	}
 
-  @Override
-  public boolean canBeCollidedWith() {
-    return true;
-  }
+	@Override
+	public boolean canBeCollidedWith() {
+		return true;
+	}
 
-  @Override
-  public boolean attackEntityFrom(DamageSource source, float amount) {
-    return !this.isInvulnerableTo(source) && parent.attackEntityFromPart(this, source, amount);
-  }
+	@Override
+	public boolean attackEntityFrom(DamageSource source, float amount) {
+		return !this.isInvulnerableTo(source) && parent.attackEntityFromPart(this, source, amount);
+	}
 
-  @Override
-  public boolean isEntityEqual(Entity entity) {
-    return this == entity || parent == entity;
-  }
+	@Override
+	public boolean isEntityEqual(Entity entity) {
+		return this == entity || parent == entity;
+	}
 
-  @Override
-  public IPacket<?> createSpawnPacket() {
-    throw new UnsupportedOperationException();
-  }
+	@Override
+	public IPacket<?> createSpawnPacket() {
+		return NetworkHooks.getEntitySpawningPacket(this);
+	}
 
-  @Override
-  public EntitySize getSize(Pose p_213305_1_) {
-    return size;
-  }
+	@Override
+	public EntitySize getSize(Pose p_213305_1_) {
+		return size;
+	}
 
 	public void setWidth(float width) {
 		setWidthAndHeight(width, size.height);
