bd6f20e527687828ad83980bdb0fa82713e96f0b
Jon McManus <due@wxwhatever.com>
2020-04-05 08:19:49 +1000

    Fix indentation issues.

--- src/main/java/twilightforest/entity/IEntityMultiPart.java
+++ src/main/java/twilightforest/entity/IEntityMultiPart.java
@@ -4,11 +4,10 @@ import net.minecraft.entity.Entity;
 import net.minecraft.util.DamageSource;
 import net.minecraft.world.World;
 
-public interface IEntityMultiPart
-{
-    World getWorld();
+public interface IEntityMultiPart {
+	World getWorld();
 
-    boolean attackEntityFromPart(MultiPartEntityPart part, DamageSource source, float damage);
+	boolean attackEntityFromPart(MultiPartEntityPart part, DamageSource source, float damage);
 
 	Entity[] getParts();
 }
