c8d3fab949baac0ef4d612b16804c1b43d113afb
pentantan <shutux500@gmail.com>
2020-05-14 22:41:18 +0900

    fixed spacing

--- src/main/java/twilightforest/util/ParticleHelper.java
+++ src/main/java/twilightforest/util/ParticleHelper.java
@@ -8,11 +8,11 @@ import net.minecraft.world.World;
 //TODO: Fix methods
 public class ParticleHelper {
 
-    public static void spawnParticles(World world, BlockPos pos, IParticleData type, int count, double speed, int... particleArgs) {
-        world.addParticle(type, pos.getX() + 0.5, pos.getY() + 0.5, pos.getZ() + 0.5, 0.5, 0.5, 0.5);
-    }
+	public static void spawnParticles(World world, BlockPos pos, IParticleData type, int count, double speed, int... particleArgs) {
+		world.addParticle(type, pos.getX() + 0.5, pos.getY() + 0.5, pos.getZ() + 0.5, 0.5, 0.5, 0.5);
+	}
 
-    public static void spawnParticles(Entity entity, IParticleData type) {
-        entity.world.addParticle(type, entity.getX(), entity.getY() + entity.getHeight() * 0.5, entity.getZ(), entity.getWidth() * 0.5, entity.getHeight() * 0.5, entity.getWidth() * 0.5);
-    }
+	public static void spawnParticles(Entity entity, IParticleData type) {
+		entity.world.addParticle(type, entity.getX(), entity.getY() + entity.getHeight() * 0.5, entity.getZ(), entity.getWidth() * 0.5, entity.getHeight() * 0.5, entity.getWidth() * 0.5);
+	}
 }
