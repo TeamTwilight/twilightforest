de0554459ab792b21bbe79be2781c7488f69f1f9
baguchan <shurf012@gmail.com>
2022-08-20 13:08:01 +0900

    changed slime beetle animation

--- src/main/java/twilightforest/client/animation/definitions/SlimeBeetleAnimations.java
+++ src/main/java/twilightforest/client/animation/definitions/SlimeBeetleAnimations.java
@@ -9,9 +9,9 @@ public class SlimeBeetleAnimations {
 	public static final AnimationDefinition AGGRESSIVE = AnimationDefinition.Builder.withLength(1.0F).looping()
 			.addAnimation("slime", new AnimationChannel(AnimationChannel.Targets.SCALE
 					, new Keyframe(0.0F, KeyframeAnimations.scaleVec(1.0F, 1.0F, 1.0F), AnimationChannel.Interpolations.CATMULLROM)
-					, new Keyframe(0.25F, KeyframeAnimations.scaleVec(1.1F, 1.1F, 1.1F), AnimationChannel.Interpolations.CATMULLROM)
+					, new Keyframe(0.25F, KeyframeAnimations.scaleVec(1.15F, 1.15F, 1.15F), AnimationChannel.Interpolations.CATMULLROM)
 					, new Keyframe(0.5F, KeyframeAnimations.scaleVec(1.0F, 1.0F, 1.0F), AnimationChannel.Interpolations.CATMULLROM)
-					, new Keyframe(0.75F, KeyframeAnimations.scaleVec(1.1F, 1.1F, 1.1F), AnimationChannel.Interpolations.CATMULLROM)
+					, new Keyframe(0.75F, KeyframeAnimations.scaleVec(1.15F, 1.15F, 1.15F), AnimationChannel.Interpolations.CATMULLROM)
 					, new Keyframe(1.0F, KeyframeAnimations.scaleVec(1.0F, 1.0F, 1.0F), AnimationChannel.Interpolations.CATMULLROM)))
 			.addAnimation("slime_center", new AnimationChannel(AnimationChannel.Targets.SCALE
 					, new Keyframe(0.0F, KeyframeAnimations.scaleVec(1.0F, 1.0F, 1.0F), AnimationChannel.Interpolations.CATMULLROM)
