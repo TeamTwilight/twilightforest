581fada7198d43a5221d171e46c4365504095172
Sleepy-Horse <kaak2008@yandex.ru>
2024-08-04 15:49:26 +0200

    Remove unused imports

--- src/main/java/twilightforest/block/entity/ReactorDebrisBlockEntity.java
+++ src/main/java/twilightforest/block/entity/ReactorDebrisBlockEntity.java
@@ -8,7 +8,6 @@ import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.phys.AABB;
-import net.minecraft.world.phys.Vec3;
 import net.minecraft.world.phys.shapes.Shapes;
 import net.minecraft.world.phys.shapes.VoxelShape;
 import org.joml.Vector3f;
