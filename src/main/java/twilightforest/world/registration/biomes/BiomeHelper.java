7fbbeb54b2b041c24970cf6731d6c40c53c2b7ae
Sleepy-Horse <97367938+Sleepy-Horse@users.noreply.github.com>
2024-10-08 14:12:26 +0200

    Remove mayapples, increase the frequency of large ferns, increase the frequency of ferns, remove double grass, increase the frequency of big rainbow trees

--- src/main/java/twilightforest/world/registration/biomes/BiomeHelper.java
+++ src/main/java/twilightforest/world/registration/biomes/BiomeHelper.java
@@ -110,7 +110,6 @@ public abstract class BiomeHelper {
 	public static BiomeGenerationSettings.Builder enchantedForestGen(HolderGetter<PlacedFeature> featureGetter, HolderGetter<ConfiguredWorldCarver<?>> carverGetter) {
 		BiomeGenerationSettings.Builder biome = new BiomeGenerationSettings.Builder(featureGetter, carverGetter);
 		BiomeDefaultFeatures.addDefaultSoftDisks(biome);
-		BiomeDefaultFeatures.addSavannaGrass(biome);
 		BiomeDefaultFeatures.addDefaultGrass(biome);
 		biome.addFeature(GenerationStep.Decoration.VEGETAL_DECORATION, VegetationPlacements.PATCH_SUGAR_CANE);
 		BiomeDefaultFeatures.addSurfaceFreezing(biome);
@@ -121,7 +120,7 @@ public abstract class BiomeHelper {
 		biome.addFeature(GenerationStep.Decoration.VEGETAL_DECORATION, TFPlacedFeatures.PLACED_DENSE_FERNS);
 		biome.addFeature(GenerationStep.Decoration.VEGETAL_DECORATION, TFPlacedFeatures.PLACED_DENSE_LARGE_FERNS);
 
-		addForestVegetation(biome);
+		biome.addFeature(GenerationStep.Decoration.VEGETAL_DECORATION, TFPlacedFeatures.PLACED_FLOWER_PLACER);
 		commonFeatures(biome);
 
 		biome.addFeature(GenerationStep.Decoration.VEGETAL_DECORATION, TFPlacedFeatures.PLACED_ENCHANTED_FOREST_TREES);
