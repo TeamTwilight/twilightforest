957716b4bed77ed9b91361a7678f5d80abd9fae0
Sleepy-Horse <97367938+Sleepy-Horse@users.noreply.github.com>
2025-03-11 08:58:49 +0100

    Revert "Add unfinished double jump modifier"
    This reverts commit ac4274de3730c931f68cc42e4422d1359c0ee4b8.

--- src/main/java/twilightforest/init/TFDataAttachments.java
+++ src/main/java/twilightforest/init/TFDataAttachments.java
@@ -24,5 +24,4 @@ public class TFDataAttachments {
 	public static final DeferredHolder<AttachmentType<?>, AttachmentType<TravellersWingsAnimAttachment>> TRAVELLERS_WINGS_ANIM = ATTACHMENT_TYPES.register("travellers_wings_anim", () -> AttachmentType.builder(TravellersWingsAnimAttachment::new).build());
 	public static final DeferredHolder<AttachmentType<?>, AttachmentType<Boolean>> TRAVELLERS_GOGGLES_RED_THREAD_VISION = ATTACHMENT_TYPES.register("travellers_goggles_red_thread_vision", () -> AttachmentType.builder(() -> true).serialize(Codec.BOOL).build());
 	public static final DeferredHolder<AttachmentType<?>, AttachmentType<Long>> LAST_TICK_WATER_WALKING = ATTACHMENT_TYPES.register("last_tick_water_walking", () -> AttachmentType.builder(() -> 0L).serialize(Codec.LONG).build());
-	public static final DeferredHolder<AttachmentType<?>, AttachmentType<Boolean>> HAS_DOUBLE_JUMP = ATTACHMENT_TYPES.register("has_double_jump", () -> AttachmentType.builder(() -> false).serialize(Codec.BOOL).build());
 }
