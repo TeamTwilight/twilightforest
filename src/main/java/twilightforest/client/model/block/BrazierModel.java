7455691c4bfa1b8645c9ab8bb41b944be0246ead
GizmoTheMoonPig <67468252+GizmoTheMoonPig@users.noreply.github.com>
2025-04-21 09:36:45 -0600

    fix brazier

--- src/main/java/twilightforest/client/model/block/BrazierModel.java
+++ src/main/java/twilightforest/client/model/block/BrazierModel.java
@@ -20,34 +20,34 @@ public class BrazierModel extends Model {
 		PartDefinition part = mesh.getRoot();
 
 		part.addOrReplaceChild("leg1", CubeListBuilder.create()
-				.texOffs(24, 13)
-				.addBox(-1.0F, -32.0F, -1.0F, 2.0F, 32.0F, 2.0F),
+				.texOffs(24, 15)
+				.addBox(-1.0F, -30.0F, -1.0F, 2.0F, 30.0F, 2.0F),
 			PartPose.offsetAndRotation(-5.0F, 24.0F, 5.0F, 0.3655F, 0.7119F, 0.5299F));
 
 		part.addOrReplaceChild("leg2", CubeListBuilder.create()
-				.texOffs(16, 13)
-				.addBox(-1.0F, -32.0F, -1.0F, 2.0F, 32.0F, 2.0F),
+				.texOffs(16, 15)
+				.addBox(-1.0F, -30.0F, -1.0F, 2.0F, 30.0F, 2.0F),
 			PartPose.offsetAndRotation(5.0F, 24.0F, 5.0F, 0.3655F, -0.7119F, -0.5299F));
 
 		part.addOrReplaceChild("leg3", CubeListBuilder.create()
-				.texOffs(8, 13)
-				.addBox(-1.0F, -32.0F, -1.0F, 2.0F, 32.0F, 2.0F),
+				.texOffs(8, 15)
+				.addBox(-1.0F, -30.0F, -1.0F, 2.0F, 30.0F, 2.0F),
 			PartPose.offsetAndRotation(-5.0F, 24.0F, -5.0F, -0.3655F, -0.7119F, 0.5299F));
 
 		part.addOrReplaceChild("leg4", CubeListBuilder.create()
-				.texOffs(0, 13)
-				.addBox(-1.0F, -32.0F, -1.0F, 2.0F, 32.0F, 2.0F),
+				.texOffs(0, 15)
+				.addBox(-1.0F, -30.0F, -1.0F, 2.0F, 30.0F, 2.0F),
 			PartPose.offsetAndRotation(5.0F, 24.0F, -5.0F, -0.3655F, 0.7119F, -0.5299F));
 
 		part.addOrReplaceChild("basket", CubeListBuilder.create()
 				.texOffs(0, 0)
 				.addBox(-4.0F, -5.0F, -4.0F, 8.0F, 5.0F, 8.0F),
-			PartPose.offsetAndRotation(0.0F, -2.0F, 0.0F, 0.0F, 0.7854F, 0.0F));
+			PartPose.offsetAndRotation(0.0F, 0.0F, 0.0F, 0.0F, 0.7854F, 0.0F));
 
 		part.addOrReplaceChild("charcoal", CubeListBuilder.create()
 				.texOffs(32, 6)
 				.addBox(-3.0F, -1.0F, -3.0F, 6.0F, 2.0F, 6.0F),
-			PartPose.offsetAndRotation(0.0F, -3.25F, 0.0F, 0.0F, 0.7854F, 0.0F));
+			PartPose.offsetAndRotation(0.0F, -1.25F, 0.0F, 0.0F, 0.7854F, 0.0F));
 
 		part.addOrReplaceChild("rope", CubeListBuilder.create()
 				.texOffs(32, 0)
