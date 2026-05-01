3b1c3cf2e8414d0dae862861462db6b5135d21b9
baguchi <shutux500@gmail.com>
2026-05-01 14:38:59 +0900

    helmet crab polished

--- src/main/java/twilightforest/client/model/entity/HelmetCrabModel.java
+++ src/main/java/twilightforest/client/model/entity/HelmetCrabModel.java
@@ -8,17 +8,16 @@ package twilightforest.client.model.entity;
 
 import com.mojang.blaze3d.vertex.PoseStack;
 import com.mojang.blaze3d.vertex.VertexConsumer;
-import net.minecraft.client.model.HierarchicalModel;
+import net.minecraft.client.model.EntityModel;
 import net.minecraft.client.model.geom.ModelPart;
 import net.minecraft.client.model.geom.PartPose;
 import net.minecraft.client.model.geom.builders.*;
 import net.minecraft.client.renderer.texture.OverlayTexture;
 import net.minecraft.util.Mth;
-import net.minecraft.world.entity.LivingEntity;
 import twilightforest.client.JappaPackReloadListener;
-import twilightforest.entity.monster.HelmetCrab;
+import twilightforest.client.state.HelmetCrabRenderState;
 
-public class HelmetCrabModel extends HierarchicalModel<HelmetCrab> {
+public class HelmetCrabModel extends EntityModel<HelmetCrabRenderState> {
 
 	private final ModelPart root;
 	private final ModelPart helmet;
@@ -32,6 +31,7 @@ public class HelmetCrabModel extends HierarchicalModel<HelmetCrab> {
 	private final ModelPart leftLeg2;
 
 	public HelmetCrabModel(ModelPart root) {
+		super(root);
 		this.root = root;
 
 		this.helmet = root.getChild("helmet_base");
@@ -214,11 +214,6 @@ public class HelmetCrabModel extends HierarchicalModel<HelmetCrab> {
 	}
 
 	@Override
-	public ModelPart root() {
-		return this.root;
-	}
-
-	@Override
 	public void renderToBuffer(PoseStack stack, VertexConsumer consumer, int light, int overlay, int color) {
 		this.crab.render(stack, consumer, light, overlay, color);
 		//no red overlay on the helmet
@@ -226,9 +221,24 @@ public class HelmetCrabModel extends HierarchicalModel<HelmetCrab> {
 	}
 
 	@Override
-	public void setupAnim(HelmetCrab entity, float limbSwing, float limbSwingAmount, float ageInTicks, float netHeadYaw, float headPitch) {
-		this.crab.yRot = netHeadYaw * Mth.DEG_TO_RAD;
-		this.body.xRot = headPitch * Mth.DEG_TO_RAD;
+	public void setupAnim(HelmetCrabRenderState entity) {
+		super.setupAnim(entity);
+		this.helmet.yRot = entity.helmetRot * Mth.DEG_TO_RAD;
+		if (entity.deathTime > 0) {
+			float f = ((float) entity.deathTime + entity.partialTick - 1.0F) / 20.0F * 1.6F;
+			f = Mth.sqrt(f);
+			if (f > 1.0F) {
+				f = 1.0F;
+			}
+			f *= entity.id % 4 >= 2 ? 1 : -1;
+
+			this.crab.zRot = (f * 90.0F) * Mth.DEG_TO_RAD;
+		} else {
+			this.crab.zRot = 0.0F;
+		}
+
+		this.crab.yRot = entity.yRot * Mth.DEG_TO_RAD;
+		this.body.xRot = entity.xRot * Mth.DEG_TO_RAD;
 
 		float f6 = (Mth.PI / 4F);
 		this.rightLeg1.zRot = -f6 * 0.74F;
@@ -241,12 +251,12 @@ public class HelmetCrabModel extends HierarchicalModel<HelmetCrab> {
 		this.leftLeg1.yRot = -f8 - f7;
 		this.rightLeg2.yRot = -f8 + f7;
 		this.leftLeg2.yRot = f8 - f7;
-		float f10 = -(Mth.cos(limbSwing * 0.6662F * 2.0F + Mth.PI) * 0.4F) * limbSwingAmount;
-		float f11 = -(Mth.cos(limbSwing * 0.6662F * 2.0F + Mth.HALF_PI) * 0.4F) * limbSwingAmount;
-		float f12 = -(Mth.cos(limbSwing * 0.6662F * 2.0F + (Mth.PI * 3.0F / 2.0F)) * 0.4F) * limbSwingAmount;
-		float f14 = Math.abs(Mth.sin(limbSwing * 0.6662F + Mth.PI) * 0.4F) * limbSwingAmount;
-		float f15 = Math.abs(Mth.sin(limbSwing * 0.6662F + Mth.HALF_PI) * 0.4F) * limbSwingAmount;
-		float f16 = Math.abs(Mth.sin(limbSwing * 0.6662F + (Mth.PI * 3.0F / 2.0F)) * 0.4F) * limbSwingAmount;
+		float f10 = -(Mth.cos(entity.walkAnimationPos * 0.6662F * 2.0F + Mth.PI) * 0.4F) * entity.walkAnimationSpeed;
+		float f11 = -(Mth.cos(entity.walkAnimationPos * 0.6662F * 2.0F + Mth.HALF_PI) * 0.4F) * entity.walkAnimationSpeed;
+		float f12 = -(Mth.cos(entity.walkAnimationPos * 0.6662F * 2.0F + (Mth.PI * 3.0F / 2.0F)) * 0.4F) * entity.walkAnimationSpeed;
+		float f14 = Math.abs(Mth.sin(entity.walkAnimationPos * 0.6662F + Mth.PI) * 0.4F) * entity.walkAnimationSpeed;
+		float f15 = Math.abs(Mth.sin(entity.walkAnimationPos * 0.6662F + Mth.HALF_PI) * 0.4F) * entity.walkAnimationSpeed;
+		float f16 = Math.abs(Mth.sin(entity.walkAnimationPos * 0.6662F + (Mth.PI * 3.0F / 2.0F)) * 0.4F) * entity.walkAnimationSpeed;
 		this.rightLeg1.yRot += f10;
 		this.leftLeg1.yRot -= f10;
 		this.rightLeg2.yRot += f11;
@@ -259,42 +269,16 @@ public class HelmetCrabModel extends HierarchicalModel<HelmetCrab> {
 		// swing right arm as if it were an arm, not a leg
 		if (JappaPackReloadListener.INSTANCE.isJappaPackLoaded()) {
 			this.rightClaw.yRot = 0.319531F;
-			this.rightClaw.yRot += (Mth.cos(limbSwing * 0.6662F + Mth.PI) * 2.0F * limbSwingAmount * 0.5F) / 2;
+			this.rightClaw.yRot += (Mth.cos(entity.walkAnimationPos * 0.6662F + Mth.PI) * 2.0F * entity.walkAnimationSpeed * 0.5F) / 2;
 			this.leftClaw.yRot = -0.319531F;
-			this.leftClaw.yRot += -(Mth.cos(limbSwing * 0.6662F + Mth.PI) * 2.0F * limbSwingAmount * 0.5F) / 2;
+			this.leftClaw.yRot += -(Mth.cos(entity.walkAnimationPos * 0.6662F + Mth.PI) * 2.0F * entity.walkAnimationSpeed * 0.5F) / 2;
 		} else {
 			this.rightClaw.yRot = -1.319531F;
-			this.rightClaw.yRot += Mth.cos(limbSwing * 0.6662F + Mth.PI) * 2.0F * limbSwingAmount * 0.5F;
+			this.rightClaw.yRot += Mth.cos(entity.walkAnimationPos * 0.6662F + Mth.PI) * 2.0F * entity.walkAnimationSpeed * 0.5F;
 			this.leftClaw.zRot = f6;
 			this.leftClaw.yRot = f8 * 2.0F - f7;
 			this.leftClaw.yRot -= f12;
 			this.leftClaw.zRot -= f16;
-		}
-	}
-
-	private float getHelmetRotation(HelmetCrab entity, float partialTicks) {
-		float f = Mth.rotLerp(partialTicks, entity.yBodyRotO, entity.yBodyRot);
-		float f1 = Mth.rotLerp(partialTicks, entity.helmetRotO, entity.helmetRot);
-		float f2 = f1 - f;
-		return Mth.wrapDegrees(f2) - 25;
-	}
-
-	@Override
-	public void prepareMobModel(HelmetCrab entity, float limbSwing, float limbSwingAmount, float partialTick) {
-		this.root().getAllParts().forEach(ModelPart::resetPose);
-		super.prepareMobModel(entity, limbSwing, limbSwingAmount, partialTick);
-		this.helmet.yRot = this.getHelmetRotation(entity, partialTick) * Mth.DEG_TO_RAD;
-		if (entity.deathTime > 0) {
-			float f = ((float)entity.deathTime + partialTick - 1.0F) / 20.0F * 1.6F;
-			f = Mth.sqrt(f);
-			if (f > 1.0F) {
-				f = 1.0F;
-			}
-			f *= entity.getId() % 4 >= 2 ? 1 : -1;
-
-			this.crab.zRot = (f * 90.0F) * Mth.DEG_TO_RAD;
-		} else {
-			this.crab.zRot = 0.0F;
 		}
 	}
 }
---
+++ src/main/java/twilightforest/client/state/HelmetCrabRenderState.java
@@ -0,0 +1,9 @@
+package twilightforest.client.state;
+
+import net.minecraft.client.renderer.entity.state.LivingEntityRenderState;
+
+public class HelmetCrabRenderState extends LivingEntityRenderState {
+	public float helmetRot;
+	public int id;
+	public boolean blue;
+}
