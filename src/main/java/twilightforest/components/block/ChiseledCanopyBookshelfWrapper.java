691ea275411bbbf48b65a0cfe1e8a29c37fe0e00
autumn <shinigami7x@proton.me>
2026-05-09 18:03:36 -0400

    Port ChiseledCanopyBookshelfWrapper to use new transfer API

--- src/main/java/twilightforest/components/block/ChiseledCanopyBookshelfWrapper.java
+++ src/main/java/twilightforest/components/block/ChiseledCanopyBookshelfWrapper.java
@@ -1,18 +1,55 @@
 package twilightforest.components.block;
 
-import net.minecraft.world.item.ItemStack;
-import net.neoforged.neoforge.items.wrapper.InvWrapper;
+import net.neoforged.neoforge.transfer.ResourceHandler;
+import net.neoforged.neoforge.transfer.item.ItemResource;
+import net.neoforged.neoforge.transfer.item.VanillaContainerWrapper;
+import net.neoforged.neoforge.transfer.transaction.TransactionContext;
+import org.jspecify.annotations.NonNull;
 import twilightforest.block.ChiseledCanopyShelfBlock;
 import twilightforest.block.entity.bookshelf.ChiseledCanopyShelfBlockEntity;
 
-public class ChiseledCanopyBookshelfWrapper extends InvWrapper {
-	public ChiseledCanopyBookshelfWrapper(ChiseledCanopyShelfBlockEntity inv) {
-		super(inv);
+public class ChiseledCanopyBookshelfWrapper implements ResourceHandler<ItemResource> {
+	private final ChiseledCanopyShelfBlockEntity container;
+	private final ResourceHandler<ItemResource> wrapped;
+
+	public ChiseledCanopyBookshelfWrapper(ChiseledCanopyShelfBlockEntity container) {
+		this.container = container;
+		this.wrapped = VanillaContainerWrapper.of(container);
 	}
 
 	@Override
-	public ItemStack extractItem(int slot, int amount, boolean simulate) {
-		if (((ChiseledCanopyShelfBlockEntity)this.getInv()).getBlockState().getValue(ChiseledCanopyShelfBlock.SPAWNER)) return ItemStack.EMPTY;
-		return super.extractItem(slot, amount, simulate);
+	public int size() {
+		return wrapped.size();
+	}
+
+	@Override
+	public @NonNull ItemResource getResource(int index) {
+		return wrapped.getResource(index);
+	}
+
+	@Override
+	public long getAmountAsLong(int index) {
+		return wrapped.getAmountAsLong(index);
+	}
+
+	@Override
+	public long getCapacityAsLong(int index, @NonNull ItemResource resource) {
+		return wrapped.getCapacityAsLong(index, resource);
+	}
+
+	@Override
+	public boolean isValid(int index, @NonNull ItemResource resource) {
+		return wrapped.isValid(index, resource);
+	}
+
+	@Override
+	public int insert(int index, @NonNull ItemResource resource, int amount, @NonNull TransactionContext transaction) {
+		return wrapped.insert(index, resource, amount, transaction);
+	}
+
+	@Override
+	public int extract(int index, @NonNull ItemResource resource, int amount, @NonNull TransactionContext transaction) {
+		if(container.getBlockState().getValue(ChiseledCanopyShelfBlock.SPAWNER)) return 0;
+		return wrapped.extract(index, resource, amount, transaction);
 	}
 }
