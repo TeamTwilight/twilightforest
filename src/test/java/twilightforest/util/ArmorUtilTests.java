3d3e4065b8683520174e3a12930d330726d6d479
a6tumn <shinigami7x@proton.me>
2026-06-09 23:35:45 -0400

    Updated ArmorUtilTests

--- src/test/java/twilightforest/util/ArmorUtilTests.java
+++ src/test/java/twilightforest/util/ArmorUtilTests.java
@@ -2,6 +2,7 @@ package twilightforest.util;
 
 import net.minecraft.core.component.DataComponentPatch;
 import net.minecraft.util.Unit;
+import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
@@ -11,8 +12,6 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import tamaized.beanification.junit.MockitoFixer;
 import twilightforest.init.TFDataComponents;
 import twilightforest.init.TFItems;
-
-import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.mock;
@@ -31,11 +30,15 @@ public class ArmorUtilTests {
 	@Test
 	public void getShroudedArmorPercentage() {
 		LivingEntity entity = mock(LivingEntity.class);
-		when(entity.getArmorSlots()).thenReturn(List.of(
-			ItemStack.EMPTY,
-			new ItemStack(Items.STICK),
-			new ItemStack(TFItems.ARCTIC_BOOTS, 1, DataComponentPatch.builder().set(TFDataComponents.EMPERORS_CLOTH.get(), Unit.INSTANCE).build())
-		));
+
+		ItemStack empty = ItemStack.EMPTY;
+		ItemStack stick = new ItemStack((Items.STICK));
+		ItemStack shrouded = new ItemStack(TFItems.ARCTIC_BOOTS, 1, DataComponentPatch.builder().set(TFDataComponents.EMPERORS_CLOTH.get(), Unit.INSTANCE).build());
+
+		when(entity.getItemBySlot(EquipmentSlot.HEAD)).thenReturn(empty);
+		when(entity.getItemBySlot(EquipmentSlot.CHEST)).thenReturn(stick);
+		when(entity.getItemBySlot(EquipmentSlot.LEGS)).thenReturn(shrouded);
+		when(entity.getItemBySlot(EquipmentSlot.FEET)).thenReturn(empty);
 
 		float result = instance.getShroudedArmorPercentage(entity);
 
