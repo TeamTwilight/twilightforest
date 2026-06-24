c25eee97cc5a48e85c6b07fde847aea72a200be2
Albazavr <aelbert.shapoval@mail.ru>
2026-06-24 16:00:36 +0300

    Utilities port

--- src/main/java/twilightforest/util/Restriction.java
+++ src/main/java/twilightforest/util/Restriction.java
@@ -1,10 +1,16 @@
 package twilightforest.util;
 
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
 import com.mojang.serialization.Codec;
+import com.mojang.serialization.Dynamic;
+import com.mojang.serialization.DynamicOps;
+import com.mojang.serialization.JsonOps;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
 import net.minecraft.core.Registry;
 import net.minecraft.core.RegistryAccess;
 import net.minecraft.core.registries.Registries;
+import net.minecraft.resources.RegistryOps;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.resources.Identifier;
 import net.minecraft.util.ExtraCodecs;
@@ -13,35 +19,41 @@ import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.biome.Biome;
 import net.minecraft.world.level.levelgen.structure.Structure;
+import org.jetbrains.annotations.NotNull;
 import twilightforest.TFRegistries;
 
 import javax.annotation.Nullable;
 import java.util.List;
 import java.util.Optional;
 
-/**
- * @param hintStructureKey ResourceKey of a structure that extends the StructureHints interface, so that the correct hint book mob spawns
- * @param enforcement      ResourceKey of the Enforcement that gets used whenever a player is in a restricted biome
- * @param multiplier       A value dictating how adverse the negative effect of a restricted area should be
- * @param lockedBiomeToast Item that is used as an icon for the notification that tells the player that the area is locked
- * @param advancements     List of advancements that are required to make a biome no longer restricted
- */
-
-public record Restriction(@Nullable ResourceKey<Structure> hintStructureKey, ResourceKey<Enforcement> enforcement,
+public record Restriction(@Nullable ResourceKey<@NotNull Structure> hintStructureKey, ResourceKey<@NotNull Enforcement> enforcement,
 						  float multiplier, @Nullable ItemStack lockedBiomeToast, List<Identifier> advancements) {
-
 	public static final Codec<Restriction> CODEC = RecordCodecBuilder.create((recordCodecBuilder) -> recordCodecBuilder.group(
 		ResourceKey.codec(Registries.STRUCTURE).optionalFieldOf("structure_key").forGetter((restriction) -> Optional.ofNullable(restriction.hintStructureKey())),
 		ResourceKey.codec(TFRegistries.Keys.ENFORCEMENT).fieldOf("enforcement").forGetter(Restriction::enforcement),
 		Codec.FLOAT.fieldOf("multiplier").forGetter(Restriction::multiplier),
-		ItemStack.CODEC.optionalFieldOf("locked_biome_toast").forGetter((restriction) -> Optional.ofNullable(restriction.lockedBiomeToast())),
+		Codec.PASSTHROUGH.optionalFieldOf("locked_biome_toast").xmap(
+			optionalDynamic -> optionalDynamic.map(dynamic -> {
+				JsonElement json = (JsonElement) dynamic.getValue();
+				DynamicOps<JsonElement> ops = RegistryOps.create(
+					JsonOps.INSTANCE,
+					RegistryAccess.EMPTY
+				);
+				return ItemStack.CODEC.parse(ops, json).result().orElse(ItemStack.EMPTY);
+			}),
+			optionalStack -> optionalStack.map(stack -> {
+				DynamicOps<JsonElement> ops = RegistryOps.create(
+					JsonOps.INSTANCE,
+					RegistryAccess.EMPTY
+				);
+				JsonElement json = ItemStack.CODEC.encodeStart(ops, stack).result().orElse(new JsonObject());
+				return new Dynamic<>(JsonOps.INSTANCE, json);
+			})
+		).forGetter((restriction) -> Optional.ofNullable(restriction.lockedBiomeToast())),
+
 		ExtraCodecs.nonEmptyList(Identifier.CODEC.listOf()).fieldOf("advancements").forGetter(Restriction::advancements)
 	).apply(recordCodecBuilder, Restriction::create));
 
-	@SuppressWarnings("OptionalUsedAsFieldOrParameterType") // Vanilla does this too
-	private static Restriction create(Optional<ResourceKey<Structure>> hintStructureKey, ResourceKey<Enforcement> enforcer, float multiplier, Optional<ItemStack> lockedBiomeToast, List<Identifier> advancements) {
-		return new Restriction(hintStructureKey.orElse(null), enforcer, multiplier, lockedBiomeToast.orElse(null), advancements);
-	}
 
 	public static Optional<Restriction> getRestrictionForBiome(Biome biome, Entity entity) {
 		if (!(entity instanceof Player player))
@@ -52,16 +64,25 @@ public record Restriction(@Nullable ResourceKey<Structure> hintStructureKey, Res
 		if (biomeLocation == null)
 			return Optional.empty();
 
-		Optional<Registry<Restriction>> restrictionsRegistry = access.lookup(TFRegistries.Keys.RESTRICTIONS);
+		Optional<Registry<@NotNull Restriction>> restrictionsRegistry = access.lookup(TFRegistries.Keys.RESTRICTIONS);
 		if (restrictionsRegistry.isEmpty())
 			return Optional.empty();
 
-		Restriction restrictions = restrictionsRegistry.get().get(biomeLocation);
-		if (restrictions == null || PlayerHelper.doesPlayerHaveRequiredAdvancements(player, restrictions.advancements())) {
-			return Optional.empty();
+		if (restrictionsRegistry.get().get(biomeLocation).isPresent()) {
+			Restriction restrictions = restrictionsRegistry.get().get(biomeLocation).get().value();
+			if (PlayerHelper.doesPlayerHaveRequiredAdvancements(player, restrictions.advancements())) {
+				return Optional.empty();
+			}
+
+			return Optional.of(restrictions);
 		}
 
-		return Optional.of(restrictions);
+		return Optional.empty();
+	}
+
+	@SuppressWarnings("OptionalUsedAsFieldOrParameterType")
+	private static Restriction create(Optional<ResourceKey<@NotNull Structure>> hintStructureKey, ResourceKey<@NotNull Enforcement> enforcer, float multiplier, Optional<ItemStack> lockedBiomeToast, List<Identifier> advancements) {
+		return new Restriction(hintStructureKey.orElse(null), enforcer, multiplier, lockedBiomeToast.orElse(null), advancements);
 	}
 
 	public static boolean isBiomeSafeFor(Biome biome, Entity entity) {
--- src/main/java/twilightforest/util/landmarks/LandmarkUtil.java
+++ src/main/java/twilightforest/util/landmarks/LandmarkUtil.java
@@ -1,8 +1,12 @@
 package twilightforest.util.landmarks;
 
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.multiplayer.ClientLevel;
 import net.minecraft.core.*;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.resources.ResourceKey;
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.server.jsonrpc.methods.GameRulesService;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.tags.TagKey;
@@ -12,14 +16,18 @@ import net.minecraft.world.level.LevelAccessor;
 import net.minecraft.world.level.LevelReader;
 import net.minecraft.world.level.chunk.ChunkAccess;
 import net.minecraft.world.level.chunk.status.ChunkStatus;
+import net.minecraft.world.level.gamerules.GameRules;
 import net.minecraft.world.level.levelgen.structure.Structure;
 import net.minecraft.world.level.levelgen.structure.StructureStart;
 import net.minecraft.world.phys.AABB;
+import net.neoforged.fml.startup.Client;
+import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
-import twilightforest.data.tags.StructureTagGenerator;
 import twilightforest.entity.EnforcedHomePoint;
 import twilightforest.init.TFAdvancements;
 import twilightforest.init.TFGameRules;
+import twilightforest.network.EnforceProgressionStatusPacket;
+import twilightforest.tags.TFStructureTags;
 import twilightforest.world.components.structures.start.TFStructureStart;
 import twilightforest.world.components.structures.util.CustomStructureData;
 
@@ -32,19 +40,19 @@ import java.util.stream.Collectors;
 @SuppressWarnings("OptionalIsPresent")
 public final class LandmarkUtil {
 	public static Optional<StructureStart> locateNearestLandmarkStart(LevelAccessor level, int chunkX, int chunkZ) {
-		return locateNearestMatchingLandmark(level, StructureTagGenerator.LANDMARK, chunkX, chunkZ);
+		return locateNearestMatchingLandmark(level, TFStructureTags.LANDMARK, chunkX, chunkZ);
 	}
 
-	public static Optional<StructureStart> locateNearestMatchingLandmark(LevelAccessor level, TagKey<Structure> matching, int chunkX, int chunkZ) {
-		var structureRegistry = level.registryAccess().registry(Registries.STRUCTURE);
+	public static Optional<StructureStart> locateNearestMatchingLandmark(LevelAccessor level, TagKey<@NotNull Structure> matching, int chunkX, int chunkZ) {
+		var structureRegistry = level.registryAccess().lookup(Registries.STRUCTURE);
 		if (structureRegistry.isEmpty()) return Optional.empty();
-		var holders = structureRegistry.get().getTag(matching);
+		var holders = structureRegistry.get().get(matching);
 		if (holders.isEmpty()) return Optional.empty();
 
 		return locateNearestMatchingLandmark(level, holders.get(), chunkX, chunkZ);
 	}
 
-	public static Optional<StructureStart> locateNearestMatchingLandmark(LevelAccessor level, HolderSet<Structure> matching, int chunkX, int chunkZ) {
+	public static Optional<StructureStart> locateNearestMatchingLandmark(LevelAccessor level, HolderSet<@NotNull Structure> matching, int chunkX, int chunkZ) {
 		Set<Structure> structures = matching.stream().map(Holder::value).collect(Collectors.toSet());
 		return locateNearestMatchingLandmark(level, structures::contains, chunkX, chunkZ, true);
 	}
@@ -70,11 +78,11 @@ public final class LandmarkUtil {
 		return start.filter(structureStart -> structureStart instanceof TFStructureStart tfStructureStart && tfStructureStart.isConquered()).isPresent();
 	}
 
-	public static void markStructureConquered(Level level, EnforcedHomePoint mobHome, ResourceKey<Structure> structureKey, boolean conquered) {
+	public static void markStructureConquered(Level level, EnforcedHomePoint mobHome, ResourceKey<@NotNull Structure> structureKey, boolean conquered) {
 		markStructureConquered(level, mobHome.getRestrictionPoint(), structureKey, conquered);
 	}
 
-	public static void markStructureConquered(Level level, @Nullable GlobalPos pos, ResourceKey<Structure> structureKey, boolean conquered) {
+	public static void markStructureConquered(Level level, @Nullable GlobalPos pos, ResourceKey<@NotNull Structure> structureKey, boolean conquered) {
 		if (pos != null && level.dimension() == pos.dimension()) {
 			Optional<StructureStart> nearStart = locateNearestLandmarkStart(level, structureKey, pos.pos());
 			if (nearStart.isEmpty() || !(nearStart.get() instanceof TFStructureStart twilightStart)) return;
@@ -88,17 +96,17 @@ public final class LandmarkUtil {
 	}
 
 	@Nullable
-	public static Structure structureForKey(LevelReader level, ResourceKey<Structure> structureKey) {
-		Optional<Registry<Structure>> registry = level.registryAccess().registry(Registries.STRUCTURE);
+	public static Structure structureForKey(LevelReader level, ResourceKey<@NotNull Structure> structureKey) {
+		Optional<Registry<@NotNull Structure>> registry = level.registryAccess().lookup(Registries.STRUCTURE);
 
-		return registry.isPresent() ? registry.get().get(structureKey) : null;
+		return registry.isPresent() ? registry.get().get(structureKey).get().value() : null;
 	}
 
-	public static Optional<StructureStart> locateNearestLandmarkStart(LevelAccessor level, ResourceKey<Structure> structureKey, BlockPos pos) {
+	public static Optional<StructureStart> locateNearestLandmarkStart(LevelAccessor level, ResourceKey<@NotNull Structure> structureKey, BlockPos pos) {
 		return locateNearestLandmarkStart(level, structureKey, SectionPos.blockToSectionCoord(pos.getX()), SectionPos.blockToSectionCoord(pos.getZ()));
 	}
 
-	public static Optional<StructureStart> locateNearestLandmarkStart(LevelAccessor level, ResourceKey<Structure> structureKey, int chunkX, int chunkZ) {
+	public static Optional<StructureStart> locateNearestLandmarkStart(LevelAccessor level, ResourceKey<@NotNull Structure> structureKey, int chunkX, int chunkZ) {
 		Structure structure = structureForKey(level, structureKey);
 
 		if (structure == null) return Optional.empty();
@@ -126,6 +134,16 @@ public final class LandmarkUtil {
 
 	public static boolean isProgressionEnforced(ServerLevel level) {
 		return level.getGameRules().get(TFGameRules.ENFORCED_PROGRESSION_RULE.get());
+	}
+
+	public static boolean isProgressionEnforced(ClientLevel level) {
+		MinecraftServer server = Minecraft.getInstance().getSingleplayerServer();
+		if (server != null) {
+			return server.getGameRules().get(TFGameRules.ENFORCED_PROGRESSION_RULE.get());
+		} else if (level.getServer() != null) {
+			return level.getServer().getGameRules().get(TFGameRules.ENFORCED_PROGRESSION_RULE.get());
+		}
+		return EnforceProgressionStatusPacket.CLIENT_ENFORCE_PROGRESSION;
 	}
 
 	private LandmarkUtil() {
