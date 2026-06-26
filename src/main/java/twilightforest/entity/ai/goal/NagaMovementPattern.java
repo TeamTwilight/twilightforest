56f5da222055df092d7f8879fc87b8c9a72b9428
Albazavr <110563872+albazavr-alba@users.noreply.github.com>
2026-06-26 11:25:04 +0300

    Update NagaMovementPattern.java

--- src/main/java/twilightforest/entity/ai/goal/NagaMovementPattern.java
+++ src/main/java/twilightforest/entity/ai/goal/NagaMovementPattern.java
@@ -68,8 +68,9 @@ public class NagaMovementPattern extends Goal {
 						this.naga.lookAt(this.naga.getTarget(), 30.0F, 30.0F);
 					}
 					this.naga.zza = 0.1f;
-					//the stunless charge has a higher chance to happen the lower the naga's health gets
 					if (!this.stunCalculated) {
+						//the stunless charge has a higher chance to happen the lower the naga's health gets
+						//difficulty is also factored in. The higher the difficulty the greater the chance
 						float healthRatio = 1.0F - (this.naga.getHealth() / (this.naga.getMaxHealth())) - 0.25F;
 						float difficultyBonus;
 
