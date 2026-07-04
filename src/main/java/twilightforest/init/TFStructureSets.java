6babb1722078a22368ede25004cfa2f534142d71
Albazavr <aelbert.shapoval@mail.ru>
2026-07-05 02:37:55 +0300

    Resolved review comment

--- src/main/java/twilightforest/init/TFStructureSets.java
+++ src/main/java/twilightforest/init/TFStructureSets.java
@@ -60,10 +60,7 @@ public class TFStructureSets {
 			new StructureSet.StructureSelectionEntry(structures.getOrThrow(TFStructures.SWAMP_HOLLOW_TREE), 1)
 		);
 
-		Holder<StructureSet> hollowTreeRef = structureSets.getOrThrow(HOLLOW_TREE);
-
-		Holder.Reference<StructureSet> fallenTrunk = context.register(FALLEN_TRUNK, new StructureSet(structures.getOrThrow(TFStructures.FALLEN_TRUNK), new AvoidLandmarkGridPlacement(Vec3i.ZERO, StructurePlacement.FrequencyReductionMethod.DEFAULT, 0.8F, 1275623845, Optional.of(new StructurePlacement.ExclusionZone(hollowTreeRef, 1)), 7, 5, RandomSpreadType.TRIANGULAR, Optional.empty())));
-		Holder.Reference<StructureSet> hollowTree = context.register(HOLLOW_TREE, new StructureSet(hollowTrees, new AvoidLandmarkGridPlacement(Vec3i.ZERO, StructurePlacement.FrequencyReductionMethod.DEFAULT, 0.5F, 34481210, Optional.empty(), 7, 5, RandomSpreadType.TRIANGULAR, Optional.empty())));
+		Holder.Reference<StructureSet> fallenTrunk = context.register(FALLEN_TRUNK, new StructureSet(structures.getOrThrow(TFStructures.FALLEN_TRUNK), new AvoidLandmarkGridPlacement(Vec3i.ZERO, StructurePlacement.FrequencyReductionMethod.DEFAULT, 0.8F, 1275623845, Optional.of(new StructurePlacement.ExclusionZone(structureSets.getOrThrow(HOLLOW_TREE), 1)), 7, 5, RandomSpreadType.TRIANGULAR, Optional.empty())));Holder.Reference<StructureSet> hollowTree = context.register(HOLLOW_TREE, new StructureSet(hollowTrees, new AvoidLandmarkGridPlacement(Vec3i.ZERO, StructurePlacement.FrequencyReductionMethod.DEFAULT, 0.5F, 34481210, Optional.empty(), 7, 5, RandomSpreadType.TRIANGULAR, Optional.empty())));
 
 		Optional<AvoidLandmarkGridPlacement.AvoidAdditionalStructures> avoidTrees = Optional.of(new AvoidLandmarkGridPlacement.AvoidAdditionalStructures(new Object2IntArrayMap<>(Map.of(
 			fallenTrunk, 2,
