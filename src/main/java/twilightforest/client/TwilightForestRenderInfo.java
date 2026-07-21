97f1109ea25248dadf82022bd0a5e2e6830bca95
Albazavr <aelbert.shapoval@mail.ru>
2026-07-21 17:43:12 +0300

    Moved changed TwilightForestRenderInfo from #2646

--- src/main/java/twilightforest/client/TwilightForestRenderInfo.java
+++ src/main/java/twilightforest/client/TwilightForestRenderInfo.java
@@ -3,26 +3,17 @@ package twilightforest.client;
 import net.minecraft.client.Camera;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.multiplayer.ClientLevel;
-import net.minecraft.client.renderer.DimensionSpecialEffects;
-import net.minecraft.client.renderer.LightTexture;
 import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.client.renderer.state.level.LevelRenderState;
 import net.minecraft.client.renderer.state.level.SkyRenderState;
 import net.minecraft.client.renderer.state.level.WeatherRenderState;
-import net.minecraft.resources.ResourceKey;
-import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.level.biome.Biome;
 import net.minecraft.world.phys.Vec3;
 import net.neoforged.neoforge.client.CustomSkyboxRenderer;
 import net.neoforged.neoforge.client.CustomWeatherEffectRenderer;
 import org.jetbrains.annotations.Nullable;
-import org.joml.Matrix4f;
 import org.joml.Matrix4fc;
 import twilightforest.client.renderer.TFSkyRenderer;
 import twilightforest.client.renderer.TFWeatherRenderer;
-import twilightforest.init.TFBiomes;
-
-import java.util.Optional;
 
 public class TwilightForestRenderInfo implements CustomSkyboxRenderer, CustomWeatherEffectRenderer {
 
@@ -43,28 +34,6 @@ public class TwilightForestRenderInfo implements CustomSkyboxRenderer, CustomWea
 //	}
 
 	@Override
-	public boolean isFoggyAt(int x, int y) { // true = nearFog
-		Player player = Minecraft.getInstance().player;
-
-		if (player != null) {
-			Optional<ResourceKey<Biome>> biome = player.level().getBiome(player.blockPosition()).unwrapKey();
-			if (biome.isPresent()) {
-				boolean spooky = biome.get() == TFBiomes.SPOOKY_FOREST;
-
-				if (player.position().y > 20 && !spooky) {
-					return false; // If player is above the dark forest then no need to make it so spooky. The darkwood leaves cover everything as low as y42.
-				}
-
-				return spooky || biome.get() == TFBiomes.DARK_FOREST || biome.get() == TFBiomes.DARK_FOREST_CENTER;
-			}
-		}
-
-		return false;
-
-		//Make the fog on these biomes much much darker, maybe pitch black even. Do we keep this harsher fog underground too?
-	}
-
-	@Override
 	public boolean renderSky(LevelRenderState levelRenderState, SkyRenderState skyRenderState, Matrix4fc modelViewMatrix, Runnable setupFog) {
 		if (this.skyRenderer == null) {
 			this.skyRenderer = new TFSkyRenderer();
@@ -74,11 +43,11 @@ public class TwilightForestRenderInfo implements CustomSkyboxRenderer, CustomWea
 
 	@Override
 	public boolean renderSnowAndRain(LevelRenderState levelRenderState, WeatherRenderState weatherRenderState, MultiBufferSource bufferSource, Vec3 camPos) {
-		return TFWeatherRenderer.renderSnowAndRain(level, ticks, partialTick, lightTexture, new Vec3(camX, camY, camZ));
+		return TFWeatherRenderer.renderSnowAndRain(Minecraft.getInstance().level, Minecraft.getInstance().levelRenderer.getTicks(), Minecraft.getInstance().getDeltaTracker().getGameTimeDeltaTicks(), camPos, bufferSource);
 	}
 
 	@Override
 	public boolean tickRain(ClientLevel level, int ticks, Camera camera) {
-		return TFWeatherRenderer.tickRain(level, ticks, camera.getBlockPosition());
+		return TFWeatherRenderer.tickRain(level, ticks, camera.blockPosition());
 	}
 }
