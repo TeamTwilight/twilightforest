ce9bc0b9872e48fc624dd1548bb7cab848336787
Albazavr <aelbert.shapoval@mail.ru>
2026-07-22 17:04:55 +0300

    Fixed MoonDialItem

--- src/main/java/twilightforest/item/MoonDialItem.java
+++ src/main/java/twilightforest/item/MoonDialItem.java
@@ -1,8 +1,6 @@
 package twilightforest.item;
 
 import net.minecraft.ChatFormatting;
-import net.minecraft.client.Minecraft;
-import net.minecraft.core.BlockPos;
 import net.minecraft.network.chat.Component;
 import net.minecraft.network.chat.MutableComponent;
 import net.minecraft.world.attribute.EnvironmentAttributes;
@@ -13,8 +11,9 @@ import net.minecraft.world.item.component.TooltipDisplay;
 import net.minecraft.world.level.Level;
 import java.time.LocalDate;
 import java.util.function.Consumer;
+
+import net.minecraft.world.level.MoonPhase;
 import org.jetbrains.annotations.Nullable;
-import twilightforest.util.MoonPhaseConverter;
 
 public class MoonDialItem extends Item {
 	public MoonDialItem(Properties properties) {
@@ -23,19 +22,18 @@ public class MoonDialItem extends Item {
 
 	@Override
 	public void appendHoverText(ItemStack stack, TooltipContext context, TooltipDisplay display, Consumer<Component> builder, TooltipFlag flag) {
-		BlockPos pos = Minecraft.getInstance().player != null ? Minecraft.getInstance().player.blockPosition() : BlockPos.ZERO;
-		builder.accept(getMoonPhase(context.level(), pos).withStyle(ChatFormatting.GRAY));
+		builder.accept(getMoonPhase(context.level()).withStyle(ChatFormatting.GRAY));
 	}
 
-	public static MutableComponent getMoonPhase(@Nullable Level level, BlockPos pos) {
+	public static MutableComponent getMoonPhase(@Nullable Level level) {
 		String phaseType;
 		if (level != null && !level.dimensionType().hasFixedTime()) {
-			var phase = level.environmentAttributes().getValue(EnvironmentAttributes.MOON_PHASE, pos);
-			phaseType = phase.getSerializedName();
+			MoonPhase phase = level.environmentAttributes().getDimensionValue(EnvironmentAttributes.MOON_PHASE);
+			phaseType = String.valueOf(phase.index());
 		} else {
 			boolean aprilFools = LocalDate.of(LocalDate.now().getYear(), 4, 1).equals(LocalDate.now());
 			phaseType = aprilFools ? "unknown_fools" : "unknown";
 		}
-		return Component.translatable("item.twilightforest.moon_dial.phase_" + MoonPhaseConverter.convertPhaseToIndex(phaseType));
+		return Component.translatable("item.twilightforest.moon_dial.phase_" + phaseType);
 	}
 }
