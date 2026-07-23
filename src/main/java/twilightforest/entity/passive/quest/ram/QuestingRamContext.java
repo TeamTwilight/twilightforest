8d4975bbdebd7703be319c710e6fe263d72b52bb
Albazavr <aelbert.shapoval@mail.ru>
2026-07-23 17:52:52 +0300

    Removed unnecessary STREAM_CODEC

--- src/main/java/twilightforest/entity/passive/quest/ram/QuestingRamContext.java
+++ src/main/java/twilightforest/entity/passive/quest/ram/QuestingRamContext.java
@@ -5,9 +5,6 @@ import com.mojang.serialization.Codec;
 import com.mojang.serialization.DataResult;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
 import net.minecraft.core.registries.Registries;
-import net.minecraft.network.RegistryFriendlyByteBuf;
-import net.minecraft.network.codec.ByteBufCodecs;
-import net.minecraft.network.codec.StreamCodec;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.world.item.DyeColor;
 import net.minecraft.world.item.Items;
@@ -15,7 +12,6 @@ import net.minecraft.world.item.crafting.Ingredient;
 import net.minecraft.world.level.storage.loot.LootTable;
 import twilightforest.loot.TFLootTables;
 
-import java.util.LinkedHashMap;
 import java.util.Map;
 
 public record QuestingRamContext(Map<DyeColor, Ingredient> questItems, ResourceKey<LootTable> lootTable) {
@@ -43,16 +39,6 @@ public record QuestingRamContext(Map<DyeColor, Ingredient> questItems, ResourceK
 		Codec.unboundedMap(DyeColor.CODEC, Ingredient.CODEC).validate(QuestingRamContext::validate).fieldOf("items").forGetter(QuestingRamContext::questItems), //FIXME: NONEMPTY_CODEC does not exist
 		ResourceKey.codec(Registries.LOOT_TABLE).fieldOf("reward").forGetter(QuestingRamContext::lootTable)
 	).apply(instance, QuestingRamContext::new));
-
-	public static final StreamCodec<RegistryFriendlyByteBuf, QuestingRamContext> STREAM_CODEC = StreamCodec.composite(
-		ByteBufCodecs.map(
-			LinkedHashMap::new,
-			DyeColor.STREAM_CODEC,
-			Ingredient.CONTENTS_STREAM_CODEC
-		), QuestingRamContext::questItems,
-		ResourceKey.streamCodec(Registries.LOOT_TABLE), QuestingRamContext::lootTable,
-		QuestingRamContext::new
-	);
 
 	private static DataResult<Map<DyeColor, Ingredient>> validate(Map<DyeColor, Ingredient> map) {
 		int colorFlags = 0;
