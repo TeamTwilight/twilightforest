147d3a918f72dc2f6f3883a08aedd6f2b2895def
Albazavr <aelbert.shapoval@mail.ru>
2026-07-24 13:14:03 +0300

    Fixed ProgressionEvents

--- src/main/java/twilightforest/events/ProgressionEvents.java
+++ src/main/java/twilightforest/events/ProgressionEvents.java
@@ -31,6 +31,7 @@ import net.neoforged.neoforge.event.entity.player.PlayerEvent;
 import net.neoforged.neoforge.event.entity.player.PlayerInteractEvent;
 import net.neoforged.neoforge.event.level.BlockEvent;
 import net.neoforged.neoforge.event.level.GameRuleChangedEvent;
+import net.neoforged.neoforge.event.level.block.BreakBlockEvent;
 import net.neoforged.neoforge.event.tick.PlayerTickEvent;
 import net.neoforged.neoforge.network.PacketDistributor;
 import net.neoforged.neoforge.server.ServerLifecycleHooks;
@@ -90,7 +91,7 @@ public class ProgressionEvents {
 	/**
 	 * Check if the player is trying to break a block in a structure that's considered unbreakable for progression reasons
 	 */
-	private void preventLockedAreaBlockBreaking(BlockEvent.BreakEvent event) {
+	private void preventLockedAreaBlockBreaking(BreakBlockEvent event) {
 		if (!(event.getLevel() instanceof ServerLevel level) || event.isCanceled()) return;
 
 		BlockPos pos = event.getPos();
