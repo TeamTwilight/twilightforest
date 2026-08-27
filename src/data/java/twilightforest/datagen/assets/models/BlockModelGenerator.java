e7f17dbbc5f4baab5dfc9643fda928140373efb1
a6tumn <shinigami7x@proton.me>
2026-08-27 01:08:24 -0400

    Fixed firefly as well.

--- src/data/java/twilightforest/datagen/assets/models/BlockModelGenerator.java
+++ src/data/java/twilightforest/datagen/assets/models/BlockModelGenerator.java
@@ -280,7 +280,7 @@ public class BlockModelGenerator extends BlockModelBuilders {
 		this.generateSpecialModel(TFBlocks.KEEPSAKE_CASKET.get(), Blocks.NETHERITE_BLOCK, block -> ItemModelUtils.specialModel(TwilightForestMod.prefix("item/keepsake_casket"), new KeepsakeCasketSpecialRenderer.Unbaked()));
 		this.generateSpecialModel(TFBlocks.SKULL_CHEST.get(), Blocks.LIGHT_GRAY_CONCRETE_POWDER, block -> ItemModelUtils.specialModel(TwilightForestMod.prefix("item/skull_chest"), new SkullChestSpecialRenderer.Unbaked()));
 		this.generateSpecialModel(TFBlocks.CICADA.get(), Blocks.SLIME_BLOCK, block -> ItemModelUtils.specialModel(TwilightForestMod.prefix("item/cicada"), new CicadaSpecialRenderer.Unbaked()));
-		this.generateSpecialModel(TFBlocks.FIREFLY.get(), Blocks.SLIME_BLOCK, block -> ItemModelUtils.specialModel(TwilightForestMod.prefix("item/firefly"), new FireflySpecialRenderer.Unbaked()));
+		this.generateSpecialModel(TFBlocks.FIREFLY.get(), Blocks.SLIME_BLOCK, block -> new AnimatedSpecialModelWrapper.Unbaked(TwilightForestMod.prefix("item/firefly"), Optional.empty(), new FireflySpecialRenderer.Unbaked()));
 		this.generateSpecialModel(TFBlocks.MOONWORM.get(), Blocks.SLIME_BLOCK, block -> new AnimatedSpecialModelWrapper.Unbaked(TwilightForestMod.prefix("item/moonworm"), Optional.empty(), new MoonwormSpecialRenderer.Unbaked()));
 
 		this.blockStateOutput.accept(MultiVariantGenerator.dispatch(TFBlocks.CANDELABRA.get())
