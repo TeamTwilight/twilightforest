c21d14140cd162b70cb16a8506e22e7a3613495f
Andromander <androsa@protonmail.com>
2026-09-08 08:59:50 +1000

    Restore some darkness in the dimension
    Made the ambient light dark again, also made Dark Forest very, very dark.

--- src/main/java/twilightforest/client/TwilightForestRenderInfo.java
+++ src/main/java/twilightforest/client/TwilightForestRenderInfo.java
@@ -32,13 +32,6 @@ public class TwilightForestRenderInfo implements CustomSkyboxRenderer, CustomWea
 	private TFSkyRenderer skyRenderer;
 
 	//TODO: Set via EnvironmentAttribute at the DimensionType level
-//	@Nullable
-//	@Override
-//	public float[] getSunriseColor(float daycycle, float partialTicks) { // Fog color
-//		return null;
-//	}
-
-	//TODO: Set via EnvironmentAttribute at the DimensionType level
 //	@Override
 //	public Vec3 getBrightnessDependentFogColor(Vec3 biomeFogColor, float daylight) { // For modifying biome fog color with daycycle
 //		return biomeFogColor.multiply(daylight * 0.94F + 0.06F, (daylight * 0.94F + 0.06F), (daylight * 0.91F + 0.09F));
