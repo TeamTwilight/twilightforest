700fbb0731ba779b23d46f8f619e5f444106e139
Autumn <shinigami7x@proton.me>
2026-09-11 10:13:52 -0400

    Applied fix.

--- src/main/java/twilightforest/block/SkullChestBlock.java
+++ src/main/java/twilightforest/block/SkullChestBlock.java
@@ -22,7 +22,6 @@ import net.minecraft.world.level.block.*;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.entity.BlockEntityTicker;
 import net.minecraft.world.level.block.entity.BlockEntityType;
-import net.minecraft.world.level.block.entity.ChestBlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.StateDefinition;
 import net.minecraft.world.level.block.state.properties.BlockStateProperties;
@@ -114,15 +113,7 @@ public class SkullChestBlock extends BaseEntityBlock implements BlockLoggingEnum
 	public BlockState playerWillDestroy(Level level, BlockPos pos, BlockState state, Player player) {
 		if (level instanceof ServerLevel sl && !player.isCreative() && sl.getGameRules().get(GameRules.BLOCK_DROPS)) {
 			BlockEntity tile = level.getBlockEntity(pos);
-			if (tile instanceof SkullChestBlockEntity chest) {
-				ItemStack stack = new ItemStack(this);
-				ItemEntity itementity = new ItemEntity(level, pos.getX(), pos.getY(), pos.getZ(), stack);
-				this.modifyDrop(state, stack);
-				if (chest.hasCustomName()) {
-					if (chest.owner != null)
-						itementity.setCustomName(chest.getDisplayName());
-					else itementity.setCustomName(chest.getCustomName());
-				}
+			if (tile instanceof SkullChestBlockEntity) {
 				if (state.getValue(BlockLoggingEnum.MULTILOGGED).getFluid() == Fluids.EMPTY) {
 					Block block = state.getValue(BlockLoggingEnum.MULTILOGGED).getBlock();
 					if (block != Blocks.AIR) {
@@ -132,8 +123,6 @@ public class SkullChestBlock extends BaseEntityBlock implements BlockLoggingEnum
 						level.addFreshEntity(item);
 					}
 				}
-				itementity.setDefaultPickUpDelay();
-				level.addFreshEntity(itementity);
 			}
 		}
 		return super.playerWillDestroy(level, pos, state, player);
