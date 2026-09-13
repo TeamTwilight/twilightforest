f35eb97bfb3e60b76ae937fc4dd24225c971d310
Tamaized <9671313+Tamaized@users.noreply.github.com>
2026-09-13 16:18:22 -0400

    Fix #2806

--- src/main/java/twilightforest/client/renderer/tooltip/TravellersBeltTooltipComponent.java
+++ src/main/java/twilightforest/client/renderer/tooltip/TravellersBeltTooltipComponent.java
@@ -14,7 +14,9 @@ import java.util.List;
 
 // modified ClientBundleTooltip
 public class TravellersBeltTooltipComponent implements ClientTooltipComponent {
-	private static final Identifier BACKGROUND_SPRITE = Identifier.withDefaultNamespace("container/bundle/background");
+	private static final Identifier SLOT_BACKGROUND_SPRITE = Identifier.withDefaultNamespace("container/bundle/slot_background");
+	private static final int SLOT_MARGIN = 4;
+	private static final int SLOT_SIZE = 24;
 	private final List<ItemStack> contents = new ArrayList<>();
 
 	public TravellersBeltTooltipComponent(TravellersArmorBeltItem.Tooltip tooltip) {
@@ -27,39 +29,32 @@ public class TravellersBeltTooltipComponent implements ClientTooltipComponent {
 
 	@Override
 	public void extractImage(Font font, int x, int y, int w, int h, GuiGraphicsExtractor guiGraphics) {
-		guiGraphics.blitSprite(RenderPipelines.GUI_TEXTURED, BACKGROUND_SPRITE, x, y, this.backgroundWidth(), this.backgroundHeight());
 		int k = 0;
 
 		for (int gridY = 0; gridY < gridSizeY(); gridY++) {
 			for (int gridX = 0; gridX < gridSizeX(); gridX++) {
-				int renderX = x + gridX * 18 + 1;
-				int renderY = y + gridY * 20 + 1;
+				int renderX = x + gridX * SLOT_SIZE;
+				int renderY = y + gridY * SLOT_SIZE;
 				this.renderSlot(renderX, renderY, k++, guiGraphics, font);
 			}
 		}
 	}
 
 	private void renderSlot(int x, int y, int itemIndex, GuiGraphicsExtractor guiGraphics, Font font) {
-		if (itemIndex >= this.contents.size()) {
-			this.blit(guiGraphics, x, y);
-		} else {
+		guiGraphics.blitSprite(RenderPipelines.GUI_TEXTURED, SLOT_BACKGROUND_SPRITE, x, y, SLOT_SIZE, SLOT_SIZE);
+		if (itemIndex < this.contents.size()) {
 			ItemStack itemstack = this.contents.get(itemIndex);
-			this.blit(guiGraphics, x, y);
-			guiGraphics.item(itemstack, x + 1, y + 1, itemIndex);
-			guiGraphics.itemDecorations(font, itemstack, x + 1, y + 1);
+			guiGraphics.item(itemstack, x + SLOT_MARGIN, y + SLOT_MARGIN, itemIndex);
+			guiGraphics.itemDecorations(font, itemstack, x + SLOT_MARGIN, y + SLOT_MARGIN);
 		}
 	}
 
-	private void blit(GuiGraphicsExtractor guiGraphics, int x, int y) {
-		guiGraphics.blitSprite(RenderPipelines.GUI_TEXTURED, Texture.SLOT.sprite, x, y, 0, Texture.SLOT.w, Texture.SLOT.h);
-	}
-
 	private int backgroundWidth() {
-		return this.gridSizeX() * 18 + 2;
+		return this.gridSizeX() * SLOT_SIZE;
 	}
 
 	private int backgroundHeight() {
-		return this.gridSizeY() * 20 + 2;
+		return this.gridSizeY() * SLOT_SIZE;
 	}
 
 	private int gridSizeX() {
@@ -78,19 +73,5 @@ public class TravellersBeltTooltipComponent implements ClientTooltipComponent {
 	@Override
 	public int getWidth(Font font) {
 		return this.backgroundWidth();
-	}
-
-	enum Texture {
-		SLOT(Identifier.withDefaultNamespace("container/bundle/slot"), 18, 20);
-
-		public final Identifier sprite;
-		public final int w;
-		public final int h;
-
-		Texture(Identifier sprite, int w, int h) {
-			this.sprite = sprite;
-			this.w = w;
-			this.h = h;
-		}
 	}
 }
