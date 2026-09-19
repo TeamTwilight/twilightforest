9b41f01acda01b9c21e2b006e06f66518d3bd08b
Drullkus <5010174+Drullkus@users.noreply.github.com>
2026-09-19 16:54:42 -0700

    Update other usages to use Maze Slime tags

--- src/main/java/twilightforest/block/MazeSlimeBlock.java
+++ src/main/java/twilightforest/block/MazeSlimeBlock.java
@@ -3,6 +3,7 @@ package twilightforest.block;
 import net.minecraft.world.level.block.SlimeBlock;
 import net.minecraft.world.level.block.state.BlockState;
 import twilightforest.init.TFBlocks;
+import twilightforest.tags.TFBlockTags;
 
 public class MazeSlimeBlock extends SlimeBlock {
 	public MazeSlimeBlock(Properties properties) {
@@ -11,7 +12,7 @@ public class MazeSlimeBlock extends SlimeBlock {
 
 	@Override
 	public boolean canStickTo(BlockState state, BlockState other) {
-		return other.is(TFBlocks.MAZE_SLIME_BLOCK) || other.is(TFBlocks.MAZESTONE);
+		return other.is(TFBlockTags.STORAGE_BLOCKS_MAZE_SLIME) || other.is(TFBlocks.MAZESTONE);
 	}
 
 	@Override
